d40e2ee3b6a8819d06389dfd61efe022e060258d
Ninh <ninh.truong@parkmanworld.com>
2016-10-24 00:40:44 +0300

    boundary refined. Demo random SPI data with 0.25x0.25 grid

---
+++ js/map.js
@@ -0,0 +1,149 @@
+// config
+var CENTER = [107.8462, 15.5776];
+var ZOOM_LEVEL = 9;
+var CREATING_AREA_FILE = false;
+
+var map;
+
+require([
+    "esri/map",
+    "esri/layers/FeatureLayer",
+    "esri/layers/CSVLayer",
+    "esri/renderers/SimpleRenderer",
+    "esri/renderers/HeatmapRenderer",
+    "esri/symbols/SimpleMarkerSymbol",
+    "esri/InfoTemplate",
+    "esri/dijit/analysis/OverlayLayers",
+    "dojo/domReady!"
+], function(Map,
+    FeatureLayer, CSVLayer,
+    SimpleRenderer, HeatmapRenderer,
+    SimpleMarkerSymbol,
+    InfoTemplate,
+    OverlayLayers) {
+
+        // todo: add the following layers:
+        // 1. VGTB river basin boundary
+        // 2. Stations location
+        // 3. SPI calculated from JAXA's grid data
+        var layerBoundary = new FeatureLayer(
+            'https://services7.arcgis.com/dzIg3kcuxPr7HZCe/arcgis/rest/services/VGTB_River_basin_boundary/FeatureServer/0',
+            {
+                class: 'layer-boundary',
+            }
+        );
+
+        var layerStations = new CSVLayer('data/stations.csv', {
+            class: 'layer-stations',
+            renderer:
+            new SimpleRenderer({
+                symbol: new SimpleMarkerSymbol({
+                    size: "16px",
+                    color: [238, 69, 0, 0.5],
+                    outline: {
+                        width: 0.5,
+                        color: "white"
+                    }
+                })
+            }),
+            infoTemplate:
+            new InfoTemplate(
+                '${name}',
+                '<p>latitude: ${lat}</p>' +
+                '<p>longitude: ${lng}</p>'
+            ),
+        });
+
+        var layerRain = new CSVLayer('data/limited.csv', {
+            class: 'layer-rain',
+            infoTemplate:
+            new InfoTemplate(
+                '${rain}',
+                '<p>latitude: ${lat}</p>' +
+                '<p>longitude: ${lng}</p>'
+            ),
+        });
+
+        var heatmapRenderer = new HeatmapRenderer({
+            field: 'rain',
+            colorStops: [
+                { value: -2, color: 'rgba(115, 0, 0, 0.5)' },
+                { value: -1.5, color: 'rgba(230, 0, 0, 0.5)' },
+                { value: -1, color: 'rgba(230, 152, 0, 0.5)' },
+                { value: -0.5, color: 'rgba(225, 211, 127, 0.5)'},
+                { value: -0.25, color: 'rgba(225, 225, 0, 0.5)'},
+                { value: 3, color: 'transparent'}
+            ],
+            blurRadius: 15,
+        });
+
+        layerRain.setRenderer(heatmapRenderer);
+
+        // map
+        map = new Map('map', {
+            basemap: 'gray',
+            zoom: ZOOM_LEVEL,
+            maxZoom: ZOOM_LEVEL,
+            minZoom: ZOOM_LEVEL,
+            slider: false,
+            logo: false,
+            center: CENTER,
+        });
+
+        map.addLayer(layerBoundary);
+        // map.addLayer(layerStations);
+        map.addLayer(layerRain);
+
+        map.on('update-end', function() {
+            map.disableMapNavigation();
+
+            $('#vgtbBoundary').append($('#' + layerBoundary.id + '_layer > path'));
+
+            if (CREATING_AREA_FILE) {
+                // create csv content
+                var csvContent = 'data:text/csv;charset=utf-8,';
+                csvContent += getVGTBPoints(map.getLayer('graphicsLayer4'), map.getLayer('graphicsLayer2'), 'csv');
+                // download csv file
+                var encodedUri = encodeURI(csvContent);
+                window.open(encodedUri);
+                var link = document.createElement('a');
+                link.setAttribute('href', encodedUri);
+                link.setAttribute('download', 'limited.csv');
+                document.body.appendChild(link); // Required for FF
+
+                link.click();
+            }
+        });
+
+    });
+
+// get points inside vgtb river basin area
+function getVGTBPoints(rainLayer, boundaryLayer, format) {
+    var boundary_polygon = boundaryLayer.graphics[0].geometry;
+
+    switch (format) {
+        case 'json':
+            var points = [];
+            rainLayer.graphics.forEach(function(item) {
+                if (boundary_polygon.contains(item.geometry)) {
+                    points.push({
+                        lat: item.attributes.lat,
+                        lng: item.attributes.lng,
+                        rain: item.attributes.rain
+                    });
+                }
+            });
+            return JSON.stringify(points);
+        case 'csv':
+            csv = 'lat,lng,rain\n';
+            rainLayer.graphics.forEach(function(item) {
+                if (boundary_polygon.contains(item.geometry)) {
+                    csv+= item.attributes.lat + ',' + item.attributes.lng + ',' + item.attributes.rain + '\n';
+                }
+            });
+            return csv;
+        default:
+            break;
+    }
+
+}
